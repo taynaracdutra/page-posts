28fc7148d7f6b4d58098b33d357e3ead9436c48a
Taynara Dutra <taynara.dutra27@gmail.com>
2022-05-19 14:43:38 -0300

    migrando para React Hooks

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,10 +1,75 @@
-import { Component } from "react";
+import { useEffect, useState, useCallback, Component } from "react";
 import "./styles.css";
 import { Posts } from "../../components/Posts";
 import { loadPosts } from "../../utils/load-posts";
 import { Button } from "../../components/Button";
 import { InputSearch } from "../../components/SearchInput";
 
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(3);
+  const [searchValue, setSearchValue] = useState("");
+
+  const noMorePosts = page + postsPerPage >= allPosts.length;
+
+  const filterPosts = !!searchValue
+    ? allPosts.filter((post) => {
+        return post.title.toUpperCase().includes(searchValue.toUpperCase());
+      })
+    : posts; // operação ternária
+
+  const handleLoadPosts = useCallback(async (page, postsPerPage) => {
+    const postsAndPhotos = await loadPosts();
+
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
+
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
+    const nextPage = page + postsPerPage;
+
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+    posts.push(...nextPosts); //spread operator
+
+    setPosts(posts);
+    setPage(nextPage);
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
+
+  return (
+    <section className="container">
+      <div className="input-container">
+        <InputSearch searchValue={searchValue} handleChange={handleChange} />
+      </div>
+
+      {filterPosts.length > 0 && <Posts posts={filterPosts} />}
+
+      {filterPosts.length === 0 && <p>Não existem posts</p>}
+
+      <div disabled className="button-container">
+        {!searchValue && ( //short-circuit js
+          <Button
+            onClick={loadMorePosts}
+            text="Reload more posts"
+            disabled={noMorePosts}
+          />
+        )}
+      </div>
+    </section>
+  );
+};
+
+/*
 export class Home extends Component {
   state = {
     posts: [],
@@ -33,7 +98,7 @@ export class Home extends Component {
     const nextPage = page + postsPerPage;
 
     const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-    posts.push(...nextPosts); /*spread operator */
+    posts.push(...nextPosts); //spread operator 
 
     this.setState({ posts, page: nextPage });
   };
@@ -52,7 +117,7 @@ export class Home extends Component {
       ? allPosts.filter((post) => {
           return post.title.toUpperCase().includes(searchValue.toUpperCase());
         })
-      : posts; /* operação ternária */
+      : posts; // operação ternária 
 
     return (
       <section className="container">
@@ -68,7 +133,7 @@ export class Home extends Component {
         {filterPosts.length === 0 && <p>Não existem posts</p>}
 
         <div disabled className="button-container">
-          {!searchValue /*short-circuit js */ && (
+          {!searchValue  && ( //short-circuit js
             <Button
               onClick={this.loadMorePosts}
               text="Reload more posts"
@@ -80,3 +145,4 @@ export class Home extends Component {
     );
   }
 }
+*/
